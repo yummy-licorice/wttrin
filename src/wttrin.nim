a2a638f69366dbb6ff0aa07a8224a24847767a5c
Luke <Infinitybeond1@protonmail.com>
2022-07-07 20:17:59 -0500

    add files

---
+++ src/wttrin.nim
@@ -0,0 +1,28 @@
+import strutils,
+       strformat,
+       httpclient
+
+## Return the temprature of the given city, 
+## if no city is given, return the weather for the current location
+proc getTemp(city: string = ""): int =
+  var client = newHttpClient()
+  var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
+  return weather.split("+")[1].strip().split("°")[0].strip().parseInt()
+
+## Get the weather condition as an emoji
+proc getCondition(city: string = ""): string =
+  var client = newHttpClient()
+  var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
+  return weather.split("+")[0].strip()
+
+## Get the unit of the weather
+## Either "C" or "F" for celcius or fahrenheit
+proc getUnit(city: string = ""): char =
+  var client = newHttpClient()
+  var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
+  if "F" in weather:
+    return 'F'
+  else:
+    return 'C'
+
+
---
+++ wttrin.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Infinitybeond1"
+description   = "A new awesome nimble package"
+license       = "GPL-3.0-or-later"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.4.8"
