04264ea48d0339934c14779b1176d15117ccfb11
Luke <88919270+Infinitybeond1@users.noreply.github.com>
2022-07-08 02:34:32 +0000

    make every function public

--- src/wttrin.nim
+++ src/wttrin.nim
@@ -4,20 +4,20 @@ import strutils,
 
 ## Return the temprature of the given city, 
 ## if no city is given, return the weather for the current location
-proc getTemp(city: string = ""): int =
+proc getTemp*(city: string = ""): int =
   var client = newHttpClient()
   var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
   return weather.split("+")[1].strip().split("°")[0].strip().parseInt()
 
 ## Get the weather condition as an emoji
-proc getCondition(city: string = ""): string =
+proc getCondition*(city: string = ""): string =
   var client = newHttpClient()
   var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
   return weather.split("+")[0].strip()
 
 ## Get the unit of the weather
 ## Either "C" or "F" for celcius or fahrenheit
-proc getUnit(city: string = ""): char =
+proc getUnit*(city: string = ""): char =
   var client = newHttpClient()
   var weather = $getContent(client ,fmt"https://wttr.in/{city}?format=1")
   if "F" in weather:
